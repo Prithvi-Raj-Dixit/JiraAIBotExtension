8164390506b7d0cb0c5ce434135482b98b68bafb
Prithvi Raj Dixit <128414757+Prithvi-Raj-Dixit@users.noreply.github.com>
2026-08-22 18:32:44 +0530

    Update content.js

--- content.js
+++ content.js
@@ -5,7 +5,7 @@
         // MULTI-ANGLE KEY RADAR: Finds Jira key anywhere on screen or in URL parameter matrices
         function findJiraKey() {
             let pathMatch = window.location.pathname.match(/browse\/([A-Z0-9\-]+)/i);
-            if (pathMatch) return pathMatch[1];
+            if (pathMatch) return pathMatch[1];   
 
             let urlParams = new URLSearchParams(window.location.search);
             let selectedIssue = urlParams.get('selectedIssue');
